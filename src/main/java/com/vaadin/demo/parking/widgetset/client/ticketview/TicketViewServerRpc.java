76c76e1ec7048dd5992f3d08b9c8dde02d8d9cc3
Tomi Virkki <virkki@vaadin.com>
2013-06-19 13:16:16 +0300

    Ticket view is non-immediate

--- src/main/java/com/vaadin/demo/parking/widgetset/client/ticketview/TicketViewServerRpc.java
+++ src/main/java/com/vaadin/demo/parking/widgetset/client/ticketview/TicketViewServerRpc.java
@@ -3,6 +3,7 @@ package com.vaadin.demo.parking.widgetset.client.ticketview;
 import java.util.List;
 
 import com.vaadin.demo.parking.widgetset.client.model.Ticket;
+import com.vaadin.shared.annotations.Delayed;
 import com.vaadin.shared.communication.ServerRpc;
 
 public interface TicketViewServerRpc extends ServerRpc {
@@ -10,5 +11,6 @@ public interface TicketViewServerRpc extends ServerRpc {
 
     void positionReceived(double latitude, double longitude);
 
+    @Delayed(lastOnly = true)
     void updateState(Ticket ticket);
 }
